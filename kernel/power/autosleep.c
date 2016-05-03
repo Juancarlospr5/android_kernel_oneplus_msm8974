729ae4a2d5da5b1b27b2d62c2d344596ca58894c
ArsenicRom <tumayordeseo5@hotmail.com>
2016-05-03 00:10:43 -0400

    Fix compilacion

--- kernel/power/autosleep.c
+++ kernel/power/autosleep.c
@@ -66,7 +66,7 @@ static DECLARE_WORK(suspend_work, try_to_suspend);
 
 void queue_up_suspend_work(void)
 {
-	if (!work_pending(&suspend_work) && autosleep_state > PM_SUSPEND_ON)
+	if (autosleep_state > PM_SUSPEND_ON)
 		queue_work(autosleep_wq, &suspend_work);
 }
 
@@ -104,15 +104,7 @@ int pm_autosleep_set_state(suspend_state_t state)
 	if (state > PM_SUSPEND_ON) {
 		pm_wakep_autosleep_enabled(true);
 		queue_up_suspend_work();
-#ifdef CONFIG_POWERSUSPEND
-		/* Yank555.lu : hook to handle powersuspend tasks (sleep) */
-		set_power_suspend_state_autosleep_hook(POWER_SUSPEND_ACTIVE);
-#endif
-#ifndef CONFIG_PM_SYNC_BEFORE_SUSPEND
-		printk(KERN_INFO "PM: Syncing filesystems ... ");
-		sys_sync();
-		printk("done.\n");
-#endif
+
 	} else {
 		pm_wakep_autosleep_enabled(false);
 	}
